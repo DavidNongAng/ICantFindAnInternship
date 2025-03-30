b87e01f0de80e4844fd0227c0a222fda16a4fb6b
davidnongang <nongangdavid@gmail.com>
2025-03-30 17:34:03 -0400

    commit #10 Updated Main page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,11 +2,37 @@
     This file is the Home Page of the application. 
 */
 
-import React from 'react';
+import React, {useState, useEffect } from 'react';
+import JobCard from '../components/JobCard';
 import '../styles/Home.css';
+import axios from 'axios';
 
 
 const Home = () => {
+
+    // State Management for Internships
+    const [internships, setInternships] = useState([]);
+
+    //
+    useEffect(() => {
+        //Fetch latest internships
+        const fetchInternships = async () => {
+            try{
+                const response = await axios.get('http://localhost:5000/api/internships/search', {
+                    params: {
+                        title: 'internship',
+                        location: 'Canada',
+                    },
+                });
+                setInternships(response.data.results || []);
+            }catch(err){
+                console.error('Failed to fetch internships ', err);
+            }
+        }
+
+        fetchInternships();
+    }, []);
+
     return(
         <main className="home-main">
             
@@ -20,6 +46,9 @@ const Home = () => {
             <section className="recent-jobs">
                 <h2 className="recent-jobs-title">Recently Added Internships</h2>
                 <div className="recent-jobs-grid">
+                    {internships.slice(0, 6).map((job, index) => (
+                        <JobCard key={index} job={job} />
+                    ))}
                 </div>
             </section>
         </main>
